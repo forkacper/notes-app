4b1e687498b4ea7e839c0b6e509f11177619cde5
Kacper Formas <kacper.formas@ageno.pl>
2021-12-15 23:51:57 +0100

    first commit

---
+++ templates/pages/create.php
@@ -0,0 +1,21 @@
+<div>
+  <h3> nowa notatka </h3>
+  <div>
+      <form class="note-form" action="/?action=create" method="post">
+          <ul>
+              <li>
+                  <label> Tytuł <span class="required">*</span></label>
+                  <input type="text" name="title" class="field-long">
+              </li>
+              <li>
+                  <labe>Opis</labe>
+                  <textarea name="description" id="field5" class="field-long field-textarea"></textarea>
+              </li>
+              <li>
+                  <input type="submit" value="Submit">
+              </li>
+          </ul>
+
+      </form>
+  </div>
+</div>
