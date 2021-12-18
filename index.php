ffbb4b94bd1bae97a9d064e43c58e3ac266fe093
Kacper Formas <kacper.formas@ageno.pl>
2021-12-18 16:44:40 +0100

    Added controller class, refactor index.php

--- index.php
+++ index.php
@@ -5,41 +5,14 @@ declare(strict_types=1);
 namespace App;
 
 require_once("src/Utils/debug.php");
-require_once("src/View.php");
+require_once("src/Controller.php");
 
 const DEFAULT_ACTION = 'list';
 
 $action = $_GET['action'] ?? DEFAULT_ACTION;
 
-$view = new View();
+$controller = new Controller();
+$controller->run($action);
 
-$viewParams = [];
 
-switch ($action) {
-    case 'create':
-        $page = 'create';
-        $created = false;
 
-        if(!empty($_POST)) {
-            $created = true;
-            $viewParams = [
-                'title' => $_POST['title'],
-                'description' => $_POST['description']
-            ];
-        }
-
-        $viewParams['created'] = $created;
-        break;
-    case 'show':
-        $viewParams = [
-            'title' => ['Moja notatka'],
-            'description' => 'Opis'
-        ];
-        break;
-    default:
-        $page = 'list';
-        $viewParams['resultList'] = "wyświetlamy notatki";
-        break;
-} 
-
-$view->render($page, $viewParams);
---
+++ src/Controller.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+require_once ("src/View.php");
+
+class Controller
+{
+    public function run(string $action): void {
+
+        $view = new View();
+        $viewParams = [];
+
+        switch ($action) {
+            case 'create':
+                $page = 'create';
+                $created = false;
+
+                if (!empty($_POST)) {
+                    $created = true;
+                    $viewParams = [
+                        'title' => $_POST['title'],
+                        'description' => $_POST['description']
+                    ];
+                }
+
+                $viewParams['created'] = $created;
+                break;
+            case 'show':
+                $viewParams = [
+                    'title' => ['Moja notatka'],
+                    'description' => 'Opis'
+                ];
+                break;
+            default:
+                $page = 'list';
+                $viewParams['resultList'] = "wyświetlamy notatki";
+                break;
+        }
+
+        $view->render($page, $viewParams);
+
+    }
+}
