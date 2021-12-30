afa40b26dd8213469257211a839a7a12ea9fc48d
Kacper Formas <kacperformas@gmail.com>
2021-12-30 22:53:10 +0100

    Refactor View class

--- src/View.php
+++ src/View.php
@@ -8,6 +8,24 @@ class View
 {
   public function render(string $page, array $params): void
   {
+
+      $params = $this->escape($params);
+
     require_once("templates/layout.php");
+  }
+
+  private function escape(array $params): array {
+      $clearParams = [];
+
+      foreach ($params as $key => $param) {
+          if(is_array($param)) {
+            $clearParams[$key] = $this->escape($param);
+          } else {
+              $clearParams[$key] = htmlentities($param);
+          }
+
+      }
+
+      return $clearParams;
   }
 }
